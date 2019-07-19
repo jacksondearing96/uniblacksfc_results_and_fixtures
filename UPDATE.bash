529d6bedd86e959d5bb5f6afba8f68a457bc1ffa
Jack <jacksondearing96@.gmail.com>
2019-07-19 15:34:01 +0930

    Test change for update script

--- UPDATE.bash
+++ UPDATE.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Create file with update instructions for VM
-echo "cd football_results_automation" >> temp_updateInstructions.txt
-echo "git fetch --all" >> temp_updateInstructions.txt
-echo "git reset --hard origin/master" >> temp_updateInstructions.txt
-echo "pm2 delete app" >> temp_updateInstructions.txt
-echo "pm2 start app.js" >> temp_updateInstructions.txt
-echo "pm2 save" >> temp_updateInstructions.txt
-echo "pm2 startup" >> temp_updateInstructions.txt
+echo "./update.bash" >> temp_updateInstructions.txt
 echo "exit" >> temp_updateInstructions.txt
 
 # Write c file to ssh into virtual machine and update website
@@ -20,7 +14,7 @@ echo "{" >> update.c
 echo '    int instructions = open("temp_updateInstructions.txt", O_RDONLY);' >> update.c
 echo "    dup2(instructions, 0);" >> update.c
 echo "    close(instructions);" >> update.c
-echo '    char* ssh = "ssh -i ""~/.ssh/JacksKeyPair.pem"" ec2-user@ec2-3-93-246-118.compute-1.amazonaws.com";' >> update.c
+echo '    char* ssh = "ssh -i ""~/.ssh/JacksKeyPair.pem"" ec2-user@ec2-3-87-221-85.compute-1.amazonaws.com";' >> update.c
 echo "    system(ssh);" >> update.c
 echo "    return 0;" >> update.c
 echo "}" >> update.c
