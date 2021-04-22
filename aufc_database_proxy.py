81408012af00340009885dc72773b3f8a69d6b02
Jackson Dearing <jacksondearing96@gmail.com>
2021-04-22 16:08:11 +1000

    Implemented actual connection with AUFC database throught mysql-connector in python. Added associated testing.

--- aufc_database_proxy.py
+++ aufc_database_proxy.py
@@ -1,208 +1,167 @@
 import json
 import util
 from datetime import datetime
-import connect_to_database
+import mysql.connector
+from database_credentials import DatabaseCredentials
+import logging
 
-last_update_time = datetime.now()
-
-def test_connection():
-    mycursor = connect_to_database.connect()
-    mycursor.execute("SELECT OppositionName, Nickname FROM oppositionclubs")
-    myresult = mycursor.fetchall()
-
-    for x in myresult:
-        print(x)
+logging.basicConfig(level=logging.INFO)
 
 
-def update_is_required():
-    global last_update_time
-    return last_update_time.date() < datetime.today().date()
+class AufcDatabaseProxy(object):
+    def __init__(self):
+        self.opposition_nicknames = None
+        self.opposition_nicknames_overrides = None
 
-def open_aufc_database():
-    # Opens the AUFC database, logs in and returns a driver for the page.
-    # Open headless chrome.
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(
-        "/Users/jacksondearing/Desktop/football_results_automation/chromedriver", chrome_options=options)
+        self.ground_nicknames = None
+        self.ground_nicknames_overrides = None
 
-    # AUFC database.
-    driver.get("http://uniblacksfc.com.au/members/index.php")
+        self.player_names_and_nicknames = None
 
-    # Fill in username and password.
-    driver.find_element_by_id('username').send_keys('bobneil')
-    driver.find_element_by_id('password').send_keys('bobneil134!')
-    login_button_selector = 'body > div > div.row > div:nth-child(2) > form > button'
-    driver.find_element_by_css_selector(login_button_selector).click()
+        self.image_url_overrides = None
 
-    return driver
+        self.connection = None
+        self.last_update_time = datetime.now()
+
+    
+    def connect_to_aufc_database(self):
+        self.connection = mysql.connector.connect(
+            host=DatabaseCredentials.host,
+            user=DatabaseCredentials.user,
+            password=DatabaseCredentials.password,
+            database=DatabaseCredentials.database)
 
 
-def update_player_names_from_database():
-    # This logs into the AUFC database front end and searches for all the registered players.
-    # It updates the .csv cached file 'registered_players.csv' with each registered player and 
-    # there nickname in the format <name>:<nickname>
-    # Storing this information in a cache greatly improves the real-time performance of the system.
-    try:
-        driver = open_aufc_database()
-
-        driver.find_element_by_id("db-menu-registration").click()
-        time.sleep(1)
-
-        # Perform an empty search to get all registered players.
-        driver.find_element_by_css_selector(
-            '#form-registration-search > button').click()
-        time.sleep(3)
-
-        names = driver.find_elements_by_class_name('registration-edit-name')
-        names = map(lambda x: x.get_attribute('innerHTML'), names)
-        nicknames = driver.find_elements_by_css_selector(
-            '.registration-edit-name + td')
-        nicknames = map(lambda x: x.get_attribute('innerHTML'), nicknames)
-
-        if (len(names) != len(nicknames)):
-            error('Scraped a different number of names and nicknames')
-
-        with open('database/registered_players.json', 'w') as file:
-            file.write('{')
-            for name, nickname in zip(names, nicknames):
-                name_parts = name.split(', ')
-                initial = name_parts[1][0] + '.'
-                full_name = name_parts[1] + ' ' + name_parts[0]
-                file.write(
-                    '"' + initial + ' ' + name_parts[0] + '":"' + str(nickname) + ' (' + full_name + ')' + '",\n')
-            file.write('}')
-
-        driver.close()
-        return True
-    except:
-        return False
-
-
-def update_nicknames_from_database():
-    # This logs into the AUFC database front end and searches for all the opposition nicknames.
-    # Updates the .csv cached file 'nicknames.csv' with each opposition nickname and 
-    # there nickname in the format <name>:<nickname>
-    # Storing this information in a cache greatly improves the real-time performance of the system.
-    try:
-        driver = open_aufc_database()
-
-        driver.find_element_by_id("db-menu-opposition").click()
-        time.sleep(1)
+    def update_cache(self):
+        if not self.update_is_required(): 
+            return
         
-        # Perform an empty search to get all registered players.
-        driver.find_element_by_css_selector(
-            '#form-opposition-search > button').click()
-        time.sleep(3)
-
-        # Get opposition names.
-        names = driver.find_elements_by_css_selector('#opposition-search-div > div > table > tbody > tr > td:nth-child(1)')
-        names = map(lambda x: x.get_attribute('innerHTML'), names)
-
-        # Get opposition nicknames.
-        nicknames = driver.find_elements_by_css_selector('#opposition-search-div > div > table > tbody > tr > td:nth-child(2)')
-        nicknames = map(lambda x: x.get_attribute('innerHTML'), nicknames)
-
-        # Verify lengths are consistent.
-        if (len(names) != len(nicknames)):
-            error('Scraped a different number of names and nicknames')
-
-        with open('database/nicknames.json', 'w') as file:
-            file.write('{')
-            for name, nickname in zip(names, nicknames):
-                name = name.replace('amp;','')
-                nickname = nickname.replace('amp;','')
-                file.write('"' + name + '":"' + nickname + '",\n')
-            file.write('}')
-
-        driver.close()
-        return True
-    except:
-        return False
-
-def update_ground_names_from_database():
-    # This logs into the AUFC database front end and searches for all the opposition ground names.
-    # Updates the .csv cached file 'ground_nicknames.csv' with each opposition ground name and 
-    # their ground nickname in the format <name>:<nickname>
-    # Storing this information in a cache greatly improves the real-time performance of the system.
-    try:
-        driver = open_aufc_database()
-
-        driver.find_element_by_id("db-menu-grounds").click()
-        time.sleep(1)
+        logging.info('Updating database cache.')
         
-        # Perform an empty search to get all registered players.
-        driver.find_element_by_css_selector(
-            '#form-ground-search > button').click()
-        time.sleep(3)
+        self.last_update_time = datetime.today()
 
-        # Get opposition ground names.
-        names = driver.find_elements_by_css_selector('#ground-search-div > div > table > tbody > tr > td:nth-child(1)')
-        names = map(lambda x: x.get_attribute('innerHTML'), names)
+        self.update_player_names_and_nicknames()
+        self.update_ground_nicknames()
+        self.update_opposition_nicknames()
 
-        # Get opposition ground nicknames.
-        nicknames = driver.find_elements_by_css_selector('#ground-search-div > div > table > tbody > tr > td:nth-child(2)')
-        nicknames = map(lambda x: x.get_attribute('innerHTML'), nicknames)
 
-        # Verify lengths are consistent.
-        if (len(names) != len(nicknames)):
-            error('Scraped a different number of names and nicknames')
-
-        with open('database/ground_nicknames.json', 'w') as file:
-            file.write('{')
-            for name, nickname in zip(names, nicknames):
-                name = name.replace('amp;','')
-                nickname = nickname.replace('amp;','')
-                file.write('"' name + '":"' + nickname + '",\n')
-            file.write('}')
-
-        driver.close()
-        return True
-    except:
+    def update_is_required(self):
+        if self.last_update_time.date() < datetime.today().date(): return True
+        if self.player_names_and_nicknames is None: return True
         return False
 
 
+    def update_overrides(self):
+        self.update_opposition_nicknames_overrides()
+        self.update_ground_nicknames_overrides()
+        self.update_image_url_overrides()
 
-def get_player_names():
-    return json.loads(util.read_file_to_string('database/registered_players.json'))
+    def update_ground_nicknames_overrides(self):
+        self.ground_nicknames_overrides = json.loads(util.read_file_to_string('database/override_ground_nicknames.json'))
+    
+    def update_image_url_overrides(self):
+        self.image_url_overrides = json.loads(util.read_file_to_string('database/override_image_urls.json'))
 
-
-def apply_overrides(nicknames, overrides):
-    for key in overrides:
-        nicknames[key] = overrides[key]
-
-
-def get_opposition_nicknames():
-    opposition_nicknames = json.loads(util.read_file_to_string('database/nicknames.json'))
-    override_opposition_nicknames = json.loads(util.read_file_to_string('database/override_nicknames.json'))
-    apply_overrides(opposition_nicknames, override_opposition_nicknames)
-    return opposition_nicknames
+    def update_opposition_nicknames_overrides(self):
+        self.opposition_nicknames_overrides = json.loads(util.read_file_to_string('database/override_nicknames.json'))
 
 
-def get_ground_nicknames():
-    ground_nicknames = json.loads(util.read_file_to_string('database/ground_nicknames.json'))
-    override_ground_nicknames = json.loads(util.read_file_to_string('database/override_ground_nicknames.json'))
-    apply_overrides(ground_nicknames, override_ground_nicknames)
-    return ground_nicknames
+    def update_player_names_and_nicknames(self):
+        self.connect_to_aufc_database()
+        cursor = self.connection.cursor()
+
+        cursor.execute('SELECT FirstName, Surname, Nickname, MemberID FROM members WHERE FirstName != ""')
+        players_and_nicknames = cursor.fetchall()
+        logging.info('Fetched {} players from AUFC database'.format(len(players_and_nicknames)))
+
+        self.player_names_and_nicknames = {}
+        for player_and_nickname in players_and_nicknames:
+            firstname = player_and_nickname[0]
+            firstname_initial = firstname[0]
+            surname = player_and_nickname[1]
+            nickname = player_and_nickname[2]
+            member_id = player_and_nickname[3]
+
+            key = firstname_initial + '. ' + surname
+            entry = { 'fullname': firstname + ' ' + surname, 'nickname': nickname, 'memberID': member_id}
+
+            # Skip this entry if we find a duplicate key (first name initial and last name).
+            # Use memberID to keep only the most recently created member by default.
+            if self.player_names_and_nicknames.has_key(key) and self.player_names_and_nicknames[key]['memberID'] > member_id:
+                logging.info('Duplicate player key found: ' + key)
+                continue
+
+            self.player_names_and_nicknames[key] = entry
+        
+        self.connection.close()
 
 
-def get_override_image_urls():
-    return json.loads(util.read_file_to_string('database/override_image_urls.json'))
+    def update_ground_nicknames(self):
+        self.update_ground_nicknames_overrides()
 
+        self.connect_to_aufc_database()
+        cursor = self.connection.cursor()
 
-def run_updates():
-    if not update_is_required(): 
-        logging.info('Updates were not required.')
+        cursor.execute('SELECT Ground, Nickname FROM grounds')
+        grounds_and_nicknames = cursor.fetchall()
 
-    if not update_ground_names_from_database():
-        return 'FAIL'
-    if not update_player_names_from_database():
-        return 'FAIL'
-    if not update_nicknames_from_database():
-        return 'FAIL'
+        self.ground_nicknames = {}
+        for ground_and_nickname in grounds_and_nicknames:
+            ground = ground_and_nickname[0]
+            nickname = ground_and_nickname[1]
 
-    global last_update_time
-    last_update_time = datetime.now()
-    return 'SUCCESS'
+            self.ground_nicknames[ground] = nickname
+
+            # Override if necessary.
+            if self.ground_nicknames_overrides.has_key(ground):
+                self.ground_nicknames[ground] = self.ground_nicknames_overrides[ground]
+        
+        self.connection.close()
+    
+    def update_opposition_nicknames(self):
+        self.update_opposition_nicknames_overrides()
+
+        self.connect_to_aufc_database()
+        cursor = self.connection.cursor()
+
+        cursor.execute("SELECT OppositionName, Nickname FROM oppositionclubs")
+        opposition_names_and_nicknames = cursor.fetchall()
+
+        self.opposition_nicknames = {}
+        for opposition_names_and_nickname in opposition_names_and_nicknames:
+
+            opposition_name = opposition_names_and_nickname[0]
+            opposition_nickname = opposition_names_and_nickname[1]
+
+            self.opposition_nicknames[opposition_name] = opposition_nickname
+
+            # Override if necessary.
+            if self.opposition_nicknames_overrides.has_key(opposition_name):
+                self.opposition_nicknames[opposition_name] = self.opposition_nicknames_overrides[opposition_name]
+        
+        self.connection.close()
+
+    def get_player_nickname(self, player_name):
+        self.update_cache()
+        if self.player_names_and_nicknames.has_key(player_name):
+            return self.player_names_and_nicknames[player_name]
+        
+        logging.warning('No match for player: {} in database.'.format(player_name))
+        return {} 
+
+    def get_opposition_nickname(self, opposition_name):
+        self.update_cache()
+        if self.opposition_nicknames.has_key(opposition_name):
+            return self.opposition_nicknames[opposition_name]
+
+        logging.warning('No match for opposition name: {} in database.'.format(opposition_name))
+        return ''
+
+    def get_ground_nickname(self, ground_name):
+        self.update_cache()
+        if self.ground_nicknames.has_key(ground_name):
+            return self.ground_nicknames[ground_name]
+
+        logging.warning('No match for ground name: {} in database.'.format(ground_name))
+        return '' 
+
--- test/test_aufcDatabaseProxy.py
+++ test/test_aufcDatabaseProxy.py
@@ -1,28 +1,82 @@
 import unittest
-import aufc_database_proxy
-import web_scraper
+import json
+
+from aufc_database_proxy import AufcDatabaseProxy
+
 
 class TestAufcDatabaseProxy(unittest.TestCase):
 
-    def test_insert_nicknames(self):
-        names_and_nicknames = aufc_database_proxy.get_player_names()
+    def setUp(self):
+        self.aufc_database = AufcDatabaseProxy()
 
-        names = 'J. Dearing'
-        names = web_scraper.insert_nicknames(names, names_and_nicknames)
-        self.assertEqual(names[0]['name'], 'Buck Hunter (Jackson Dearing)')
-        self.assertEqual(names[0]['goals'], '')
 
-        names = 'J. Dearing 7, E. Dadds'
-        names = web_scraper.insert_nicknames(names, names_and_nicknames)
-        self.assertEqual(names[0]['name'], 'Buck Hunter (Jackson Dearing)')
-        self.assertEqual(names[0]['goals'], ' 7')
-        self.assertEqual(names[1]['name'], 'Ya Mumms ya Dadds (Edward Dadds)')
-        self.assertEqual(names[1]['goals'], '')
+    def test_connect_to_database(self):
+        self.aufc_database.connect_to_aufc_database()
+        self.assertIsNotNone(self.aufc_database.connection)
+        self.aufc_database.connection.close()
 
-        names = ''
-        names = web_scraper.insert_nicknames(names, names_and_nicknames)
-        self.assertEqual(names, [])
 
-        names = None
-        names = web_scraper.insert_nicknames(names, names_and_nicknames)
-        self.assertEqual(names, [])
+    def test_update_overrides(self):
+        self.aufc_database.update_overrides()
+
+        self.assertIsNotNone(self.aufc_database.opposition_nicknames_overrides)
+        self.assertEquals(self.aufc_database.opposition_nicknames_overrides['Christies Beach'], "Christie's a B!tch")
+
+        self.assertIsNotNone(self.aufc_database.ground_nicknames_overrides)
+        self.assertEquals(self.aufc_database.ground_nicknames_overrides['Caterer Oval'], 'Hackney High')
+
+        self.assertIsNotNone(self.aufc_database.image_url_overrides)
+        self.assertEquals(self.aufc_database.image_url_overrides['Marion'], 'https://aflnational.com/wp-content/uploads/Marion-Rams-AFL-logo.jpg')
+
+
+    def test_update_opposition_nicknames(self):
+        self.aufc_database.connect_to_aufc_database()
+
+        self.aufc_database.update_opposition_nicknames()
+
+        self.assertIsNotNone(self.aufc_database.opposition_nicknames)
+        self.assertEquals(self.aufc_database.opposition_nicknames['Portland'], 'Flagon of Portland')
+        self.assertEquals(self.aufc_database.opposition_nicknames['North Haven'], 'North Haven for Hobos')
+        
+        self.aufc_database.connection.close()
+
+    def test_update_ground_nicknames(self):
+        self.aufc_database.connect_to_aufc_database()
+
+        self.aufc_database.update_ground_nicknames()
+
+        self.assertIsNotNone(self.aufc_database.ground_nicknames)
+        self.assertEquals(self.aufc_database.ground_nicknames['Woodville Oval'], 'The Woods')
+        self.assertEquals(self.aufc_database.ground_nicknames['University Oval'], 'Bob Neil #1')
+
+        self.aufc_database.connection.close()
+    
+    def test_update_player_names_and_nicknames(self):
+        self.aufc_database.connect_to_aufc_database()
+
+        self.aufc_database.update_player_names_and_nicknames()
+
+        self.assertIsNotNone(self.aufc_database.player_names_and_nicknames)
+        self.assertGreater(len(self.aufc_database.player_names_and_nicknames.keys()), 4000)
+
+        player = self.aufc_database.player_names_and_nicknames['J. Dearing']
+        self.assertEquals(player['fullname'], 'Jackson Dearing')
+        self.assertEquals(player['nickname'], 'Buck Hunter')
+
+        self.aufc_database.connection.close()
+
+    def test_get_player_nickname(self):
+        player = self.aufc_database.get_player_nickname('J. Dearing')
+        self.assertEquals(player['fullname'], 'Jackson Dearing')
+        self.assertEquals(player['nickname'], 'Buck Hunter')
+
+        self.assertEquals(self.aufc_database.get_player_nickname('J. GooseInvalid'), {})
+
+    def test_get_opposition_nickname(self):
+        self.assertEquals(self.aufc_database.get_opposition_nickname('Athelstone'), 'The Raggies')
+        self.assertEquals(self.aufc_database.get_opposition_nickname('Non existant club'), '')
+
+    def test_get_ground_nickname(self):
+        self.assertEquals(self.aufc_database.get_ground_nickname('University Oval'), 'Bob Neil #1')
+        self.assertEquals(self.aufc_database.get_ground_nickname('Goodwood Oval'), 'Sin Stadium')
+        self.assertEquals(self.aufc_database.get_ground_nickname('Non existant Oval'), '')
